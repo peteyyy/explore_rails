11ccccfc4ba1a0877a9091b798aae836c5388dfd
peteyyy <lexsp25@gmail.com>
2023-04-26 17:48:49 -0400

    Seed File Added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create(name: "Arbys", address: "2312 Dickerson Pike", state: "FL", photo_id: "680ptwxpj9uCtkAqFO--wA", business_id: "bBDDEgkFA1Otx9Lfe7BZUQ")
+Place.create(name: "Mcdonalds", address: "2312 Dickerson Pike", state: "FL", photo_id: "yvHPh_cf4opSfQdTbWAcSQ", business_id: "ljxNT9p0y7YMPx0fcNBGig")
+Place.create(name: "ChickFilA", address: "2312 Dickerson Pike", state: "FL", photo_id: "aVfRECEoZA7-Hla76J_Eew", business_id: "9OG5YkX1g2GReZM0AskizA")
+Place.create(name: "Burger King", address: "2312 Dickerson Pike", state: "FL", photo_id: "ZXMF6AHcxI9m03nFrJbpZQ", business_id: "CF33F8-E6oudUQ46HnavjQ")
+Place.create(name: "Taco Bell", address: "2312 Dickerson Pike", state: "FL", photo_id: "N3tTxsgpFH91wzo4gZm19A", business_id: "MTSW4McQd7CbVtyjqoe9mw")
+Place.create(name: "Wendys", address: "2312 Dickerson Pike", state: "FL", photo_id: "MKA75vYrSLhVz3Yvk3RQWw", business_id: "0bPLkL0QhhPO5kt1_EXmNQ")
+Place.create(name: "White Castle", address: "2312 Dickerson Pike", state: "FL", photo_id: "MKA75vYrSLhVz3Yvk3RQWw", business_id: "WKMJwqnfZKsAae75RMP6jA")
